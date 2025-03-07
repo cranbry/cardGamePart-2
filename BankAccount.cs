f89961fc3ea30868ea6b8fb3651654ff3b21db09
Mouad Oulouali <mouadoune430@gmail.com>
2025-03-06 21:11:33 -0500

    bank account class

---
+++ BankAccount.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+public class BankAccount
+{
+    // creating variables needed
+    private string accountId;
+    private string customerName;
+    private decimal balance;
+    private List<string> transactions;
+}
