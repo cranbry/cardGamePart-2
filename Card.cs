8cbf4cf896562a9636f8091cdd69fde3b494f61f
Mouad Oulouali <mouadoune430@gmail.com>
2025-03-06 20:17:09 -0500

    initial commit

---
+++ Card.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Card
+{
+    //Fields, example: Rank rank;
+    private int Rank rank;
+    private int Suit suit;
+    private bool faceUp;
+    //check the help documentation for the fields
+
+    //Card Constructor
+
+
+    //Define properties for all above fields
+    //code example: public Suit Suit { get { return suit; } }
+
+    public void FlipOver()
+    {
+        if (!faceUp)
+        {
+            
+        }
+        else
+        {
+            
+        }
+    }
+
+}
---
+++ Deck.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Deck
+{
+
+    //Deck Constructor
+    public Deck()
+    {
+        foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+        {
+            foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+            {
+
+            }
+        }
+    }
+
+    //Implement a property to get Cards
+    public List<Card> Cards
+    {
+        get
+        {
+            return cards;
+        }
+    }
+
+    //Takes top card from deck (if deck is not empty, otherwise return null)
+    public Card TakeTopCard()
+    {
+
+    }
+
+    //Shuffle Method
+    public void Shuffle()
+    {
+        
+ 
+    }
+
+    //Cut method
+    public void Cut(int index)
+    {
+        //implementation
+    }
+}
+
